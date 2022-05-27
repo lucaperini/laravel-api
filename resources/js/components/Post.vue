1f5e0305c5f864b6925dd226aee51310ea6625da
lucaperini <luca.perini@gmail.com>
2022-05-27 08:42:32 +0200

    first commit

---
+++ resources/js/components/Post.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="col-4">
+    <div class="card">
+        <img :src="post.image_url" class="card-img-top" style="width:18rem;" :alt="'image of ' + post.title">
+        <div class="card-body">
+            <div class="card-title">
+                {{post.title}}
+            </div>
+            <div class="subtitle">
+                {{post.author}}
+            </div>
+            <div class="card-text">
+                {{post.content}}
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+    name: "Post",
+    components: {
+    },
+    props:['post']
+}
+</script>
+
+<style>
+
+</style>
+
+
---
+++ resources/js/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Post v-for="(post, index) in posts" :key="index" :post="post"/>
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+    name: "PostList",
+    components: {
+        Post
+    },
+    data: function() {
+        return {
+            posts: [],
+        }
+    },
+    methods: {
+        getPosts(){
+            axios.get("http://localhost:8000/api/posts")
+            .then((result) =>{
+
+                this.posts = result.data.data;
+            })
+            .catch( (error) =>{
+                console.warn(error);
+            });
+        }
+    },
+    created(){
+        this.getPosts();
+    }
+}
+</script>
